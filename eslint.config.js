ea271638896f22feac8e561108231842df8bee85
Erick Kenji Sugahara <erick.sugahara@gmail.com>
2026-01-17 15:28:17 -0300

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,20 @@
 const expoConfig = require('eslint-config-expo/flat');
 const { defineConfig } = require('eslint/config');
 
-module.exports = defineConfig([expoConfig]);
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: [
+      'jest.config.js',
+      'jest.setup.js',
+      'tsconfig.json',
+      'tsconfig.test.json',
+      '.expo',
+      'node_modules',
+      'coverage',
+      'assets',
+      '*.json',
+      '*config.js',
+    ],
+  },
+]);
--- jest.setup.js
+++ jest.setup.js
@@ -1,24 +1,34 @@
+/* eslint-disable no-undef */
 // jest.setup.js
 const React = require('react');
 
 // Mock React Native
 jest.mock('react-native', () => {
   return {
-    View: React.forwardRef((props, ref) =>
-      React.createElement('div', { ref, ...props }),
-    ),
-    Text: React.forwardRef((props, ref) =>
-      React.createElement('span', { ref, ...props }),
-    ),
+    View: React.forwardRef(function View(props, ref) {
+      const View = viewProps => React.createElement('div', viewProps);
+      View.displayName = 'MockView';
+      return React.createElement(View, { ...props, ref });
+    }),
+    Text: React.forwardRef(function Text(props, ref) {
+      const Text = textProps => React.createElement('span', textProps);
+      Text.displayName = 'MockText';
+      return React.createElement(Text, { ...props, ref });
+    }),
     StyleSheet: {
       create: styles => styles,
     },
-    TextInput: React.forwardRef((props, ref) =>
-      React.createElement('input', { ref, ...props }),
-    ),
-    TouchableOpacity: React.forwardRef((props, ref) =>
-      React.createElement('div', { ref, ...props }),
-    ),
+    TextInput: React.forwardRef(function TextInput(props, ref) {
+      const TextInput = inputProps => React.createElement('input', inputProps);
+      TextInput.displayName = 'MockTextInput';
+      return React.createElement(TextInput, { ...props, ref });
+    }),
+    TouchableOpacity: React.forwardRef(function TouchableOpacity(props, ref) {
+      const TouchableOpacity = touchProps =>
+        React.createElement('div', touchProps);
+      TouchableOpacity.displayName = 'MockTouchableOpacity';
+      return React.createElement(TouchableOpacity, { ...props, ref });
+    }),
   };
 });
 
